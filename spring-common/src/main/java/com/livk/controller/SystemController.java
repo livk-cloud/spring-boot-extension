6adfbe8c22aa13356f62b7660e0876da06b18ae1
livk1375632510 <chenyu1999>
2022-02-14 11:06:55 +0800

    feat

---
+++ spring-common/src/main/java/com/livk/controller/SystemController.java
@@ -0,0 +1,27 @@
+package com.livk.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * SystemController
+ * </p>
+ *
+ * @author livk
+ * @date 2022/2/12
+ */
+@RestController
+@RequiredArgsConstructor
+public class SystemController {
+
+    private final ApplicationContext applicationContext;
+
+    @PostMapping("shutdown")
+    public void shutdown(){
+        System.exit(SpringApplication.exit(applicationContext));
+    }
+}
