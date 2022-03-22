5106951764297916b37388ccb86ea682549e82c4
livk1375632510 <chenyu1999>
2022-03-22 10:28:57 +0800

    feat

--- spring-redis-demo/spring-redis-lua/src/main/java/com/livk/redis/controller/BuyController.java
+++ spring-redis-demo/spring-redis-lua/src/main/java/com/livk/redis/controller/BuyController.java
@@ -1,8 +1,11 @@
 package com.livk.redis.controller;
 
+import com.livk.common.redis.supprot.LivkRedisTemplate;
 import com.livk.redis.support.LuaStock;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,9 +25,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class BuyController {
 
     private final LuaStock luaStock;
+    private final LivkRedisTemplate livkRedisTemplate;
+
+    @PostConstruct
+    public void init() {
+        if (Boolean.TRUE.equals(livkRedisTemplate.hasKey("livk"))) {
+            livkRedisTemplate.delete("livk");
+        }
+        livkRedisTemplate.opsForValue().set("livk", 1);
+    }
 
     @PostMapping("buy")
     public HttpEntity<String> buy() {
         return ResponseEntity.ok(luaStock.buy(1));
+    }
+
+    @PostMapping("put")
+    public void put() {
+        ValueOperations<String, Object> value = livkRedisTemplate.opsForValue();
+        if ((Integer) value.get("livk") > 0) {
+            value.decrement("livk");
+        }
     }
 }
