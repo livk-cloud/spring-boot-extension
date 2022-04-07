a238754cba42faade496d8d53b222aec27959a9b
livk1375632510 <chenyu1999>
2022-04-07 16:01:20 +0800

    feat

--- spring-redis-demo/spring-redis-provider/src/main/java/com/livk/redis/controller/MessageController.java
+++ spring-redis-demo/spring-redis-provider/src/main/java/com/livk/redis/controller/MessageController.java
@@ -4,6 +4,8 @@ import com.livk.common.redis.domain.LivkMessage;
 import com.livk.common.redis.supprot.LivkReactiveRedisTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -30,6 +32,16 @@ public class MessageController {
                            @RequestBody Map<String, Object> data) {
         return livkReactiveRedisTemplate
                 .convertAndSend(LivkMessage.CHANNEL, LivkMessage.of().setId(id).setMsg(msg).setData(data))
+                .flatMap(n -> Mono.empty());
+    }
+
+    @PostMapping("/redis/stream")
+    public Mono<Void> stream() {
+        ObjectRecord<String, String> record = StreamRecords.newRecord()
+                .ofObject("livk-object")
+                .withStreamKey("livk-streamKey");
+        return livkReactiveRedisTemplate.opsForStream()
+                .add(record)
                 .flatMap(n -> Mono.empty());
     }
 }
