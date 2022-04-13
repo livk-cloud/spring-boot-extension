1b0d846b419be11fff3120b7752c0f79bc8d4116
livk1375632510 <chenyu1999>
2022-04-13 10:49:38 +0800

    feat

---
+++ spring-rabbit-demo/mqtt-demo/src/main/java/com/livk/mqtt/config/MqttConfig.java
@@ -0,0 +1,38 @@
+package com.livk.mqtt.config;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+
+/**
+ * <p>
+ * MqttConfig
+ * </p>
+ *
+ * @author livk
+ * @date 2022/3/3
+ */
+@Configuration
+@EnableConfigurationProperties(MqttProperties.class)
+public class MqttConfig {
+    @Bean
+    public MqttConnectOptions mqttConnectOptions(MqttProperties properties) {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(properties.getUsername());
+        options.setPassword(properties.getPassword().toCharArray());
+        options.setServerURIs(properties.getUrl().split(";"));
+        options.setConnectionTimeout(properties.getConnectTimeout());
+        options.setKeepAliveInterval(properties.getKeepAliveInterval());
+        return options;
+    }
+
+    @Bean
+    public MqttPahoClientFactory mqttPahoClientFactory(MqttConnectOptions mqttConnectOptions) {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setConnectionOptions(mqttConnectOptions);
+        return factory;
+    }
+}
---
+++ spring-rabbit-demo/mqtt-demo/src/main/java/com/livk/mqtt/config/MqttProperties.java
@@ -0,0 +1,40 @@
+package com.livk.mqtt.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * <p>
+ * MqttProperties
+ * </p>
+ *
+ * @author livk
+ * @date 2022/3/3
+ */
+@Data
+@ConfigurationProperties(MqttProperties.MQTT)
+public class MqttProperties {
+
+    public static final String MQTT = "spring.mqtt";
+
+    private String username;
+
+    private String password;
+
+    private String url;
+
+    private int connectTimeout = 10;
+
+    private int keepAliveInterval = 20;
+
+    private Meta sender;
+
+    private Meta receiver;
+
+    @Data
+    public static class Meta{
+        private String clientId;
+
+        private String defaultTopic;
+    }
+}
