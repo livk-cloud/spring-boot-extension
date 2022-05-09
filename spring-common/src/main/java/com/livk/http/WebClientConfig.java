3c3aa4eb631400f4f2b67e86d06ba3b363b23d07
livk <1375632510@qq.com>
2022-05-09 09:49:38 +0800

    feat

---
+++ spring-common/src/main/java/com/livk/http/WebClientConfig.java
@@ -0,0 +1,31 @@
+package com.livk.http;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * <p>
+ * WebClientConfig
+ * </p>
+ *
+ * @author livk
+ * @date 2022/5/9
+ */
+@Configuration
+public class WebClientConfig {
+
+    /**
+     * spring官方建议使用{@link WebClient} <br>
+     * <a href="https://docs.spring.io/spring-framework/docs/current/reference/html/web.html#webmvc-client">Spring文档</a>
+     * @return WebClient
+     */
+    @Bean
+    @ConditionalOnClass(WebClient.class)
+    @ConditionalOnMissingBean
+    public WebClient webClient() {
+        return WebClient.create();
+    }
+}
