25380d1d557a336ddc1eecc3e84275bb677afa81
livk <1375632510@qq.com>
2022-05-13 15:23:58 +0800

    feat

--- buildSrc/src/main/groovy/com/livk/plugin/ResourcesPlugin.groovy
+++ buildSrc/src/main/groovy/com/livk/plugin/ResourcesPlugin.groovy
@@ -5,6 +5,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.compile.JavaCompile
+
 /**
  * <p>
  * ResourcesPlugin
@@ -16,6 +17,8 @@ import org.gradle.api.tasks.compile.JavaCompile
 class ResourcesPlugin implements Plugin<Project> {
 
     private static final List<String> COMPILER_ARGS = new ArrayList<>()
+    private static final String MAPSTRUCT_NAME = "mapstruct"
+    private static final String MAPSTRUCT_COMPILER_ARGS = "-Amapstruct.unmappedTargetPolicy=IGNORE";
 
     static {
         COMPILER_ARGS.addAll(Arrays.asList(
@@ -33,5 +36,16 @@ class ResourcesPlugin implements Plugin<Project> {
                 .dependsOn(Format.NAME, JavaPlugin.PROCESS_RESOURCES_TASK_NAME) as JavaCompile
         javaCompile.options.compilerArgs.addAll(COMPILER_ARGS)
         javaCompile.options.encoding = "UTF-8"
+        javaCompile.doFirst {
+            def dependencyName = new HashSet<>()
+            project.configurations.forEach {
+                dependencyName.addAll(it.dependencies.name)
+            }
+            if (dependencyName.contains(MAPSTRUCT_NAME)) {
+                if (!javaCompile.options.compilerArgs.contains(MAPSTRUCT_COMPILER_ARGS)) {
+                    javaCompile.options.compilerArgs.add(MAPSTRUCT_COMPILER_ARGS)
+                }
+            }
+        }
     }
 }
