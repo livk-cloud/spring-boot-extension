414df198b55361b04f8900cdb9fad81487f49260
livk <1375632510@qq.com>
2022-05-19 14:53:11 +0800

    feat

--- spring-mapstruct-demo/mapstruct-starter/src/main/java/com/livk/mapstruct/support/MapstructService.java
+++ spring-mapstruct-demo/mapstruct-starter/src/main/java/com/livk/mapstruct/support/MapstructService.java
@@ -1,7 +1,7 @@
 package com.livk.mapstruct.support;
 
+import com.livk.mapstruct.exception.ConverterNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 
 import java.util.Collection;
 import java.util.Objects;
@@ -15,32 +15,30 @@ import java.util.stream.Stream;
  * @author livk
  * @date 2022/5/11
  */
-@SuppressWarnings({ "unchecked", "rawtypes" })
+@SuppressWarnings({"unchecked", "rawtypes"})
 @RequiredArgsConstructor
 public class MapstructService {
 
-	private final ConverterRepository repository;
+    private final ConverterRepository repository;
 
-	public <T> T converter(Object source, Class<T> targetClass) {
-		Class<?> sourceClass = source.getClass();
-		if (repository.contains(sourceClass, targetClass)) {
-			return (T) Objects.requireNonNull(repository.get(sourceClass, targetClass)).getTarget(source);
-		}
-		else if (repository.contains(targetClass, sourceClass)) {
-			return (T) Objects.requireNonNull(repository.get(targetClass, sourceClass)).getSource(source);
-		}
-		return BeanUtils.instantiateClass(targetClass);
-	}
+    public <T> T converter(Object source, Class<T> targetClass) {
+        Class<?> sourceClass = source.getClass();
+        if (repository.contains(sourceClass, targetClass)) {
+            return (T) Objects.requireNonNull(repository.get(sourceClass, targetClass)).getTarget(source);
+        } else if (repository.contains(targetClass, sourceClass)) {
+            return (T) Objects.requireNonNull(repository.get(targetClass, sourceClass)).getSource(source);
+        }
+        throw new ConverterNotFoundException(source + " to class " + targetClass + " not found converter");
+    }
 
-	public <C extends Collection, T> Stream<T> converter(C sources, Class<T> targetClass) {
-		Class<?> sourceClass = sources.stream().distinct().findFirst().orElseThrow().getClass();
-		if (repository.contains(sourceClass, targetClass)) {
-			return (Stream<T>) Objects.requireNonNull(repository.get(sourceClass, targetClass)).streamTarget(sources);
-		}
-		else if (repository.contains(targetClass, sourceClass)) {
-			return (Stream<T>) Objects.requireNonNull(repository.get(targetClass, sourceClass)).streamSource(sources);
-		}
-		return Stream.empty();
-	}
+    public <C extends Collection, T> Stream<T> converter(C sources, Class<T> targetClass) {
+        Class<?> sourceClass = sources.stream().distinct().findFirst().orElseThrow().getClass();
+        if (repository.contains(sourceClass, targetClass)) {
+            return (Stream<T>) Objects.requireNonNull(repository.get(sourceClass, targetClass)).streamTarget(sources);
+        } else if (repository.contains(targetClass, sourceClass)) {
+            return (Stream<T>) Objects.requireNonNull(repository.get(targetClass, sourceClass)).streamSource(sources);
+        }
+        throw new ConverterNotFoundException(sources + " to class " + targetClass + " not found converter");
+    }
 
 }
