2f04212c71d320a4f52c09cb3fa9ea30189487bc
livk <1375632510@qq.com>
2022-06-08 14:43:49 +0800

    feat

--- buildSrc/src/main/groovy/com/livk/plugin/DependencyBomPlugin.groovy
+++ buildSrc/src/main/groovy/com/livk/plugin/DependencyBomPlugin.groovy
@@ -3,7 +3,6 @@ package com.livk.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.plugins.JavaPluginExtension
 
 /**
  * <p>
@@ -20,18 +19,17 @@ abstract class DependencyBomPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def dependencyBom = project.configurations.create(DEPENDENCY_BOM)
+        dependencyBom.visible = false
         dependencyBom.canBeResolved = false
         dependencyBom.canBeConsumed = false
         project.plugins.withType(JavaPlugin.class) {
-            project.extensions.getByType(JavaPluginExtension.class).sourceSets.every {
-                project.configurations.getByName(JavaPlugin.IMPLEMENTATION_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-                project.configurations.getByName(JavaPlugin.API_ELEMENTS_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-                project.configurations.getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-                project.configurations.getByName(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-                project.configurations.getByName(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-                project.configurations.getByName(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-                project.configurations.getByName(JavaPlugin.TEST_ANNOTATION_PROCESSOR_CONFIGURATION_NAME).extendsFrom(dependencyBom)
-            }
+            project.configurations.getByName(JavaPlugin.IMPLEMENTATION_CONFIGURATION_NAME).extendsFrom(dependencyBom)
+            project.configurations.getByName(JavaPlugin.API_ELEMENTS_CONFIGURATION_NAME).extendsFrom(dependencyBom)
+            project.configurations.getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom(dependencyBom)
+            project.configurations.getByName(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME).extendsFrom(dependencyBom)
+            project.configurations.getByName(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME).extendsFrom(dependencyBom)
+            project.configurations.getByName(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom(dependencyBom)
+            project.configurations.getByName(JavaPlugin.TEST_ANNOTATION_PROCESSOR_CONFIGURATION_NAME).extendsFrom(dependencyBom)
         }
     }
 }
