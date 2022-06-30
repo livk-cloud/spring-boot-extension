7e493fd5d122a54b9fd244f82450105c100dd229
livk <1375632510@qq.com>
2022-06-30 10:11:58 +0800

    feat

--- spring-mybatis-demo/mybatis-example/src/main/resources/table.sql
+++
@@ -1,8 +0,0 @@
-create table user
-(
-    `id`          int not null auto_increment primary key,
-    `username`    varchar(20),
-    `version`     int,
-    `insert_time` datetime,
-    `update_time` datetime
-)
--- spring-redis-demo/redis-lua/src/main/resources/hash.lua
+++
@@ -1,6 +0,0 @@
-local result = redis.call('hmset', KEYS[1], 'code', ARGV[1], 'expire', ARGV[2])
-if result['ok'] == 'OK' then
-    return 0
-else
-    return 1
-end
