34f1fcfd4f6e346fab2b51d46cac2407e88a59ee
livk <1375632510@qq.com>
2022-08-26 10:19:31 +0800

    feat

--- spring-redis/spring-redisson/src/main/java/com/livk/redisson/schedule/ScheduleHandler.java
+++ spring-redis/spring-redisson/src/main/java/com/livk/redisson/schedule/ScheduleHandler.java
@@ -3,6 +3,7 @@ package com.livk.redisson.schedule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RScheduledExecutorService;
+import org.redisson.api.RScheduledFuture;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.WorkerOptions;
 import org.springframework.stereotype.Component;
@@ -27,9 +28,16 @@ public class ScheduleHandler {
 
     public void start() {
         RScheduledExecutorService executorService = redissonClient.getExecutorService("livk");
-        executorService.registerWorkers(WorkerOptions.defaults());
-        executorService.schedule((Runnable & Serializable) () -> log.info("66666"), 5, TimeUnit.SECONDS);
-        System.out.println(executorService.getTaskIds());
-        executorService.delete();
+        executorService.registerWorkers(WorkerOptions.defaults().workers(12));
+        RScheduledFuture<?> future = executorService.scheduleAtFixedRate((Runnable & Serializable) () -> {
+            log.info("time:{}", System.currentTimeMillis());
+            try {
+                Thread.sleep(10000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+        log.info("TaskId:{}", future.getTaskId());
+//        executorService.delete();
     }
 }
