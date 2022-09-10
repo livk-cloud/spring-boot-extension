fe267dce1fc7e25d01bf4188c765a55023bc08ed
livk <1375632510@qq.com>
2022-09-10 22:02:18 +0800

    refactor:

--- spring-http/http-spring-boot-autoconfigure/src/main/java/com/livk/http/factory/HttpServiceRegistrar.java
+++ spring-http/http-spring-boot-autoconfigure/src/main/java/com/livk/http/factory/HttpServiceRegistrar.java
@@ -4,12 +4,12 @@ import com.livk.http.annotation.BeanName;
 import com.livk.util.SpringUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.support.*;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -30,15 +30,25 @@ import java.util.Set;
  * @date 2022/5/20
  */
 @RequiredArgsConstructor
-public class HttpServiceRegistrar implements BeanFactoryPostProcessor, ResourceLoaderAware {
+public class HttpServiceRegistrar implements BeanDefinitionRegistryPostProcessor, ResourceLoaderAware, BeanFactoryAware {
 
     private final HttpServiceProxyFactory proxyFactory;
 
     private ResourceLoader resourceLoader;
 
+    private BeanFactory beanFactory;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
+    }
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         List<String> packages = AutoConfigurationPackages.get(beanFactory);
         Set<Class<?>> typesAnnotatedClass = SpringUtils.findByAnnotationType(HttpExchange.class, resourceLoader,
                 packages.toArray(String[]::new));
@@ -46,8 +56,8 @@ public class HttpServiceRegistrar implements BeanFactoryPostProcessor, ResourceL
             BeanDefinition beanDefinition = getBeanDefinition(exchangeClass);
             BeanName name = AnnotationUtils.getAnnotation(exchangeClass, BeanName.class);
             String beanName = name != null ? name.value()
-                    : BeanDefinitionReaderUtils.generateBeanName(beanDefinition, defaultListableBeanFactory);
-            defaultListableBeanFactory.registerBeanDefinition(beanName, beanDefinition);
+                    : BeanDefinitionReaderUtils.generateBeanName(beanDefinition, registry);
+            registry.registerBeanDefinition(beanName, beanDefinition);
         }
     }
 
