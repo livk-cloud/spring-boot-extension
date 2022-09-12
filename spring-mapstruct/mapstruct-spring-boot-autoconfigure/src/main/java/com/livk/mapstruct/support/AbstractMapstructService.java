4d5e8a436a3d259e8ca0c6d9092d07524d5035fa
livk <1375632510@qq.com>
2022-09-12 14:26:21 +0800

    feat

--- spring-mapstruct/mapstruct-spring-boot-autoconfigure/src/main/java/com/livk/mapstruct/support/AbstractMapstructService.java
+++ spring-mapstruct/mapstruct-spring-boot-autoconfigure/src/main/java/com/livk/mapstruct/support/AbstractMapstructService.java
@@ -4,9 +4,6 @@ import com.livk.mapstruct.converter.MapstructService;
 import com.livk.mapstruct.exception.ConverterNotFoundException;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Collection;
-import java.util.stream.Stream;
-
 /**
  * <p>
  * AbstractMapstructService
@@ -29,13 +26,5 @@ public abstract class AbstractMapstructService implements MapstructService {
             return (T) converterRepository.get(targetClass, sourceClass).getSource(source);
         }
         throw new ConverterNotFoundException(source + " to class " + targetClass + " not found converter");
-    }
-
-    @Override
-    public <S, T> Stream<T> converter(Collection<S> sources, Class<T> targetClass) {
-        if (sources == null || sources.isEmpty()) {
-            return Stream.empty();
-        }
-        return sources.stream().map(source -> converter(source, targetClass));
     }
 }
