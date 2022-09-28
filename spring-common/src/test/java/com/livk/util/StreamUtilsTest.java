76388505f9bbaf4bb12732b7855407cc82489bda
livk <1375632510@qq.com>
2022-09-28 17:32:49 +0800

    feat

--- spring-common/src/test/java/com/livk/util/StreamUtilsTest.java
+++ spring-common/src/test/java/com/livk/util/StreamUtilsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * <p>
@@ -24,4 +26,24 @@ class StreamUtilsTest {
                 List.of("123456"), "host", List.of("192.168.1.1"));
         Assertions.assertEquals(listMap, result);
     }
+
+    @Test
+    void testDistinct() {
+        List<Integer> list = Stream.of(1, 1, 2, 2, 3, 3, 4, 4)
+                .filter(StreamUtils.distinct(Function.identity()))
+                .toList();
+        Assertions.assertEquals(list, List.of(1, 2, 3, 4));
+
+        List<User> users = Stream.of(new User(1, "1"), new User(1, "11"),
+                        new User(2, "2"), new User(2, "2"))
+                .filter(StreamUtils.distinct(User::id))
+                .toList();
+        Assertions.assertEquals(users, List.of(new User(1, "1"), new User(2, "2")));
+    }
+
+    private static record User(Integer id, String username) {
+
+    }
 }
+
+
