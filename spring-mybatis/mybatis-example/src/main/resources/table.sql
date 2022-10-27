6631416e0a619d980ac5cf3003c72c8573085eec
livk <1375632510@qq.com>
2022-10-27 15:01:45 +0800

    test: spring-mybatis test

--- spring-mybatis/mybatis-example/src/main/resources/table.sql
+++ spring-mybatis/mybatis-example/src/main/resources/table.sql
@@ -1,6 +1,8 @@
-create table user
+DROP TABLE IF EXISTS `user`;
+
+create table `user`
 (
-    `id`          int not null auto_increment primary key,
+    `id`          int not null primary key,
     `username`    varchar(20),
     `version`     int,
     `insert_time` datetime,
--- spring-mybatis/mybatis-example/src/test/java/com/livk/example/mapper/UserMapperTest.java
+++ spring-mybatis/mybatis-example/src/test/java/com/livk/example/mapper/UserMapperTest.java
@@ -5,8 +5,7 @@ import com.github.pagehelper.PageHelper;
 import com.livk.common.LivkPage;
 import com.livk.example.entity.User;
 import com.livk.util.FieldUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,26 +17,43 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @author livk
  * @date 2022/7/26
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 class UserMapperTest {
     @Autowired
     UserMapper userMapper;
 
+    Integer id = 10;
+
+
+    @Order(1)
+    @Test
+    public void saveTest() {
+        User user = new User();
+        user.setId(id);
+        user.setUsername("livk");
+        int result = userMapper.insert(user);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Order(2)
     @Test
     public void updateTest() {
         User user = new User();
-        user.setId(3);
+        user.setId(id);
         user.setUsername("livk https");
         int result = userMapper.updateById(user);
         Assertions.assertEquals(1, result);
     }
 
+    @Order(3)
     @Test
     public void selectByIdTest() {
-        User result = userMapper.selectById(2);
+        User result = userMapper.selectById(id);
         Assertions.assertNotNull(result);
     }
 
+    @Order(4)
     @Test
     public void selectAllTest() {
         try (Page<User> page = PageHelper.<User>startPage(1, 10)
@@ -48,9 +64,10 @@ class UserMapperTest {
         }
     }
 
+    @Order(5)
     @Test
     public void deleteTest() {
-        int result = userMapper.deleteById(5);
+        int result = userMapper.deleteById(id);
         Assertions.assertEquals(1, result);
     }
 }
