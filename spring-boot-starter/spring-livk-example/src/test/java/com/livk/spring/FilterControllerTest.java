9bd9c81c6a38e2d2259845add78a077e44297150
livk <1375632510@qq.com>
2022-10-27 14:30:22 +0800

    test: spring batch

--- spring-boot-starter/spring-livk-example/src/test/java/com/livk/spring/FilterControllerTest.java
+++ spring-boot-starter/spring-livk-example/src/test/java/com/livk/spring/FilterControllerTest.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -32,6 +32,6 @@ class FilterControllerTest {
         mockMvc.perform(get("/tenant").header(TenantContextHolder.ATTRIBUTES, "livk"))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.content().string("livk"));
+                .andExpect(content().string("livk"));
     }
 }
