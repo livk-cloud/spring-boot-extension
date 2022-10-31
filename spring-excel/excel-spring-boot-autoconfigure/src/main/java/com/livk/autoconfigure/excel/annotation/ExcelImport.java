135b510daabb603ce81cc7e2ae05fedae43feb54
livk <1375632510@qq.com>
2022-10-31 14:26:17 +0800

    docs: excel注解文档补充

--- spring-excel/excel-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/excel/annotation/ExcelImport.java
+++ spring-excel/excel-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/excel/annotation/ExcelImport.java
@@ -9,9 +9,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>
- * ImportExcel
- * </p>
+ * <p>在Mvc的环境下支持{@link java.util.Collection}</p>
+ * <p>{@example List<?>}</p>
+ * <p></p><p>在Reactive的环境下支持
+ * {@link java.util.Collection}、</p>
+ * <p>{@link reactor.core.publisher.Mono}</p>
+ * <p>{@example List<?>}</p>
+ * <p>{@example Mono<List < ?>>}</p>
  *
  * @author livk
  * @date 2022/1/19
--- spring-excel/excel-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/excel/annotation/ExcelReturn.java
+++ spring-excel/excel-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/excel/annotation/ExcelReturn.java
@@ -9,9 +9,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>
- * ExcelReturn
- * </p>
+ * <p>在Mvc的环境下支持{@link java.util.Collection}、</p>
+ * <p>{@link java.util.Map}</p>
+ * <p>{@example List<?>}</p>
+ * <p>{@example Map<String, List < ?>>}</p>
+ * <p></p>
+ * <p>在Reactive的环境下支持{@link java.util.Collection}、</p>
+ * <p>{@link java.util.Map}、</p>
+ * <p>{@link reactor.core.publisher.Mono}、</p>
+ * <p>{@link reactor.core.publisher.Flux} </p>
+ * <p>{@example List<?>}</p>
+ * <p>{@example Map<String, List < ?>>}</p>
+ * <p>{@example Mono<List < ?>>}</p>
+ * <p>{@example Mono<Map < String, List < ?>>>}</p>
+ * <p>{@example Flux<?>}</p>
  *
  * @author livk
  * @date 2022/1/19
