0503c32b46897e93fad339983bdd26c2d74f45f5
livk <1375632510@qq.com>
2022-10-31 17:57:11 +0800

    refactor: lock

---
+++ spring-lock/lock-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/lock/LockAutoConfiguration.java
@@ -0,0 +1,46 @@
+package com.livk.autoconfigure.lock;
+
+import com.livk.autoconfigure.lock.aspect.LockAspect;
+import com.livk.autoconfigure.lock.local.LocalLock;
+import com.livk.autoconfigure.lock.redis.RedissonLock;
+import com.livk.autoconfigure.lock.support.DistributedLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * <p>
+ * LockAutoConfiguration
+ * </p>
+ *
+ * @author livk
+ * @date 2022/10/31
+ */
+@AutoConfiguration
+public class LockAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LockAspect lockAspect(ObjectProvider<DistributedLock> distributedLockProvider) {
+        return new LockAspect(distributedLockProvider);
+    }
+
+    @Bean
+    public DistributedLock localLock() {
+        return new LocalLock();
+    }
+
+    @ConditionalOnClass(RedissonClient.class)
+    @AutoConfiguration(afterName = "com.livk.autoconfigure.redisson.RedissonAutoConfiguration")
+    public static class RedissonLockAutoConfiguration {
+        @Bean
+        @ConditionalOnBean(RedissonClient.class)
+        public DistributedLock redissonLock(RedissonClient redissonClient) {
+            return new RedissonLock(redissonClient);
+        }
+    }
+}
