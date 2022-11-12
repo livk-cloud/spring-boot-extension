4dc10923997ea359f9d0833dbe4682ca635c1130
livk <1375632510@qq.com>
2022-11-12 16:10:46 +0800

    feat

--- spring-admin/spring-admin-server/src/main/java/com/livk/admin/server/SpringServerApp.java
+++ spring-admin/spring-admin-server/src/main/java/com/livk/admin/server/SpringServerApp.java
@@ -1,6 +1,5 @@
 package com.livk.admin.server;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.livk.spring.LivkSpring;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
@@ -8,9 +7,7 @@ import de.codecentric.boot.admin.server.domain.values.Registration;
 import de.codecentric.boot.admin.server.utils.jackson.RegistrationDeserializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
@@ -29,20 +26,15 @@ public class SpringServerApp {
     }
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer customizer() {
-        return builder -> {
-            SimpleModule simpleModule = new SimpleModule();
-            simpleModule.addDeserializer(Registration.class, new RegistrationDeserializer());
-            builder.build().registerModule(simpleModule);
-        };
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        return new HttpMessageConverters(new RegistrationHttpMessageConverters());
     }
 
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
-        SimpleModule simpleModule = new SimpleModule();
-        simpleModule.addDeserializer(Registration.class, new RegistrationDeserializer());
-        objectMapper.registerModule(simpleModule);
-        return new HttpMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper));
+    public static class RegistrationHttpMessageConverters extends MappingJackson2HttpMessageConverter {
+        public RegistrationHttpMessageConverters() {
+            SimpleModule simpleModule = new SimpleModule();
+            simpleModule.addDeserializer(Registration.class, new RegistrationDeserializer());
+            getObjectMapper().registerModule(simpleModule);
+        }
     }
 }
--- spring-redis/redis-common/src/main/java/com/livk/common/redis/util/SerializerUtils.java
+++ spring-redis/redis-common/src/main/java/com/livk/common/redis/util/SerializerUtils.java
@@ -3,6 +3,9 @@ package com.livk.common.redis.util;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.livk.support.SpringContextHolder;
 import lombok.experimental.UtilityClass;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -23,7 +26,11 @@ public class SerializerUtils {
     }
 
     public <T> RedisSerializer<T> json(Class<T> tClass) {
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = SpringContextHolder.getBeanProvider(ObjectMapper.class).getIfUnique();
+        if (mapper == null) {
+            mapper = JsonMapper.builder().build();
+            mapper.registerModule(new JavaTimeModule());
+        }
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         return json(tClass, mapper);
     }
