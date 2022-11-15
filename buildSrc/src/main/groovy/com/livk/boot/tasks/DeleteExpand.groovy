5253750ed8a03eacaa1b1dd576d17030c6a5d312
livk <1375632510@qq.com>
2022-11-15 11:04:25 +0800

    refactor: DeleteExpand重构

--- buildSrc/src/main/groovy/com/livk/boot/tasks/DeleteExpand.groovy
+++ buildSrc/src/main/groovy/com/livk/boot/tasks/DeleteExpand.groovy
@@ -3,6 +3,7 @@ package com.livk.boot.tasks
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Delete
+import org.gradle.language.base.plugins.LifecycleBasePlugin
 
 /**
  * <p>
@@ -14,12 +15,18 @@ import org.gradle.api.tasks.Delete
  */
 abstract class DeleteExpand implements Plugin<Project> {
 
+    public static final String CLEAN_GENERATED_TASK_NAME = "cleanGenerated"
+
     @Override
     void apply(Project project) {
         project.tasks.withType(Delete.class) {
             it.delete(project.projectDir.absolutePath + "/build")
             it.delete(project.projectDir.absolutePath + "/out")
             it.delete(project.projectDir.absolutePath + "/bin")
+        }.forEach(delete -> delete.dependsOn(CLEAN_GENERATED_TASK_NAME))
+
+        project.tasks.create(CLEAN_GENERATED_TASK_NAME, Delete.class) {
+            setGroup(LifecycleBasePlugin.BUILD_GROUP)
             it.delete(project.projectDir.absolutePath + "/src/main/generated")
             it.delete(project.projectDir.absolutePath + "/src/test/generated_tests")
         }
