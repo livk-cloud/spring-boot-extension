7d48040c4de49c49b0ce4da9f4ef70441bf572b4
livk <1375632510@qq.com>
2022-11-18 15:55:01 +0800

    refactor:

--- buildSrc/src/main/groovy/com/livk/boot/tasks/DeleteExpand.groovy
+++ buildSrc/src/main/groovy/com/livk/boot/tasks/DeleteExpand.groovy
@@ -17,18 +17,28 @@ abstract class DeleteExpand implements Plugin<Project> {
 
     public static final String CLEAN_GENERATED_TASK_NAME = "cleanGenerated"
 
+    public static final String CLEAN_OTHER_TASK_NAME = "cleanOther"
+
+    public static final String CLEAN_ALL_TASK_NAME = "cleanAll"
+
     @Override
     void apply(Project project) {
-        project.tasks.withType(Delete.class) {
-            it.delete(project.projectDir.absolutePath + "/build")
-            it.delete(project.projectDir.absolutePath + "/out")
-            it.delete(project.projectDir.absolutePath + "/bin")
-        }.forEach(delete -> delete.dependsOn(CLEAN_GENERATED_TASK_NAME))
-
         project.tasks.create(CLEAN_GENERATED_TASK_NAME, Delete.class) {
             setGroup(LifecycleBasePlugin.BUILD_GROUP)
             it.delete(project.projectDir.absolutePath + "/src/main/generated")
             it.delete(project.projectDir.absolutePath + "/src/test/generated_tests")
         }
+
+        project.tasks.create(CLEAN_OTHER_TASK_NAME, Delete.class) {
+            setGroup(LifecycleBasePlugin.BUILD_GROUP)
+            it.delete(project.projectDir.absolutePath + "/build")
+            it.delete(project.projectDir.absolutePath + "/out")
+            it.delete(project.projectDir.absolutePath + "/bin")
+        }
+
+        project.tasks.create(CLEAN_ALL_TASK_NAME, Delete.class) {
+            setGroup(LifecycleBasePlugin.BUILD_GROUP)
+        }.dependsOn(CLEAN_GENERATED_TASK_NAME, CLEAN_OTHER_TASK_NAME)
+
     }
 }
