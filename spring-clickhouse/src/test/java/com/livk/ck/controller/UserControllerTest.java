817e5f7e8e82949d054ffb39c7387f420c4667ff
livk <1375632510@qq.com>
2022-11-18 15:49:43 +0800

    test:

---
+++ spring-clickhouse/src/test/java/com/livk/ck/controller/UserControllerTest.java
@@ -0,0 +1,72 @@
+package com.livk.ck.controller;
+
+import com.livk.ck.entity.User;
+import com.livk.util.DateUtils;
+import com.livk.util.JacksonUtils;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * <p>
+ * UserControllerTest
+ * </p>
+ *
+ * @author livk
+ * @date 2022/11/18
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Order(2)
+    @Test
+    void testList() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Order(3)
+    @Test
+    void testRemove() throws Exception {
+        String format = DateUtils.format(LocalDateTime.now(), "yyyy-MM-dd");
+        mockMvc.perform(delete("/user/" + format))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Order(1)
+    @Test
+    void testSave() throws Exception {
+        User user = new User().setId(Integer.MAX_VALUE)
+                .setAppId("appId")
+                .setVersion("version")
+                .setRegTime(new Date());
+        mockMvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JacksonUtils.toJsonStr(user)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+}
