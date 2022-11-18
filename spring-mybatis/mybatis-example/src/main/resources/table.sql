662228d0851cd747da919c11c73bbefec27e1c13
livk <1375632510@qq.com>
2022-11-18 20:26:41 +0800

    feat: sql

--- spring-mybatis/mybatis-example/src/main/resources/table.sql
+++ spring-mybatis/mybatis-example/src/main/resources/table.sql
@@ -1,8 +1,17 @@
-create table `user`
+create table if not exists `user`
 (
-    `id`          int not null primary key,
-    `username`    varchar(20),
-    `version`     int,
+    `id`
+    int
+    not
+    null
+    primary
+    key,
+    `username`
+    varchar
+(
+    20
+),
+    `version` int,
     `insert_time` datetime,
     `update_time` datetime
-);
+    );
