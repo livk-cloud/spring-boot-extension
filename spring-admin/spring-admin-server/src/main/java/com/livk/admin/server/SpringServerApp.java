71b6eb9e1289c68a02f198b086fb8eaa4efbeea1
livk <1375632510@qq.com>
2022-11-18 11:45:06 +0800

    refactor:

--- spring-admin/spring-admin-server/src/main/java/com/livk/admin/server/SpringServerApp.java
+++ spring-admin/spring-admin-server/src/main/java/com/livk/admin/server/SpringServerApp.java
@@ -1,14 +1,8 @@
 package com.livk.admin.server;
 
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.livk.spring.LivkSpring;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import de.codecentric.boot.admin.server.domain.values.Registration;
-import de.codecentric.boot.admin.server.utils.jackson.RegistrationDeserializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
  * <p>
@@ -23,18 +17,5 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 public class SpringServerApp {
     public static void main(String[] args) {
         LivkSpring.run(SpringServerApp.class, args);
-    }
-
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        return new HttpMessageConverters(new RegistrationHttpMessageConverters());
-    }
-
-    public static class RegistrationHttpMessageConverters extends MappingJackson2HttpMessageConverter {
-        public RegistrationHttpMessageConverters() {
-            SimpleModule simpleModule = new SimpleModule();
-            simpleModule.addDeserializer(Registration.class, new RegistrationDeserializer());
-            getObjectMapper().registerModule(simpleModule);
-        }
     }
 }
---
+++ spring-admin/spring-admin-server/src/main/java/com/livk/admin/server/converter/RegistrationHttpMessageConverters.java
@@ -0,0 +1,30 @@
+package com.livk.admin.server.converter;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.utils.jackson.AdminServerModule;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * RegistrationHttpMessageConverters
+ * </p>
+ *
+ * @author livk
+ * @date 2022/11/18
+ */
+@Component
+public class RegistrationHttpMessageConverters extends HttpMessageConverters {
+
+    public RegistrationHttpMessageConverters(AdminServerProperties adminServerProperties) {
+        super(jacksonHttpMessageConverter(adminServerProperties));
+    }
+
+    private static HttpMessageConverter<?> jacksonHttpMessageConverter(AdminServerProperties adminServerProperties) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.getObjectMapper().registerModule(new AdminServerModule(adminServerProperties.getMetadataKeysToSanitize()));
+        return converter;
+    }
+}
