f139ff76a9ba0602a07499e28b7d7fc99e155ebe
livk <1375632510@qq.com>
2022-12-06 17:18:43 +0800

    refactor:

--- buildSrc/src/main/groovy/com/livk/boot/info/ManifestPlugin.groovy
+++ buildSrc/src/main/groovy/com/livk/boot/info/ManifestPlugin.groovy
@@ -5,7 +5,6 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.util.GradleVersion
-
 /**
  * <p>
  * JarPlugin
@@ -19,16 +18,13 @@ abstract class ManifestPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.pluginManager.apply(JavaPlugin.class)
-        (project.tasks.named(JavaPlugin.JAR_TASK_NAME).get() as Jar)
-                .manifest.attributes(
-                [
-                        "Implementation-Group"  : project.group,
-                        "Implementation-Title"  : project.name,
-                        "Implementation-Version": project.version,
-                        "Created-By"            : System.getProperty("java.version") + " (" +
-                                (System.getProperty("java.specification.vendor")) + ")",
-                        "Gradle-Version"        : GradleVersion.current()
-                ]
-        )
+        project.tasks.withType(Jar.class).configureEach {
+            def attributes = it.manifest.attributes
+            attributes.putIfAbsent("Implementation-Group", project.group)
+            attributes.putIfAbsent("Implementation-Title", project.name)
+            attributes.putIfAbsent("Implementation-Version", project.version)
+            attributes.putIfAbsent("Created-By", System.getProperty("java.version") + " (" + System.getProperty("java.specification.vendor") + ")")
+            attributes.putIfAbsent("Gradle-Version", GradleVersion.current())
+        }
     }
 }
