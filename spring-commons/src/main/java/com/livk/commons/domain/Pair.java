db00d6330e5b12893888368e6d7cb1d4455e3b47
livk <1375632510@qq.com>
2022-12-28 14:45:48 +0800

    fix: 删除Jackson序列化与反序列化组件

--- spring-commons/src/main/java/com/livk/commons/domain/Pair.java
+++ spring-commons/src/main/java/com/livk/commons/domain/Pair.java
@@ -1,20 +1,7 @@
 package com.livk.commons.domain;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
-import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
-
-import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * <p>
@@ -23,8 +10,6 @@ import java.util.Map;
  *
  * @author livk
  */
-@JsonSerialize(using = Pair.PairJsonSerializer.class)
-@JsonDeserialize(using = Pair.PairJsonDeserializer.class)
 public record Pair<K, V>(K key, V value) implements Serializable, Cloneable {
 
     public static final Pair<?, ?> EMPTY = Pair.of(null, null);
@@ -42,38 +27,6 @@ public record Pair<K, V>(K key, V value) implements Serializable, Cloneable {
             return (Pair<K, V>) super.clone();
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
-        }
-    }
-
-    static class PairJsonSerializer extends StdScalarSerializer<Pair<?, ?>> {
-
-        protected PairJsonSerializer() {
-            super(Pair.class, false);
-        }
-
-        @Override
-        public void serialize(Pair<?, ?> value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            gen.writeStartObject(value);
-            gen.writeObjectField(value.key.toString(), value.value);
-            gen.writeEndObject();
-        }
-    }
-
-    static class PairJsonDeserializer extends StdScalarDeserializer<Pair<?, ?>> {
-
-        protected PairJsonDeserializer() {
-            super(Pair.class);
-        }
-
-        @Override
-        public Pair<?, ?> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-            JsonNode jsonNode = ctxt.readTree(p);
-            Iterator<Map.Entry<String, JsonNode>> iterator = jsonNode.fields();
-            if (iterator.hasNext()) {
-                Map.Entry<String, JsonNode> next = iterator.next();
-                return Pair.of(next.getKey(), next.getValue());
-            }
-            return Pair.EMPTY;
         }
     }
 }
