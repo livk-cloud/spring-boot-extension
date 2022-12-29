06a3ad7009f405c33a86ba490ba16fdd1f585221
livk <1375632510@qq.com>
2022-12-29 11:50:49 +0800

    feat: pair序列化与反序列化

--- spring-commons/src/main/java/com/livk/commons/domain/Pair.java
+++ spring-commons/src/main/java/com/livk/commons/domain/Pair.java
@@ -1,5 +1,17 @@
 package com.livk.commons.domain;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
+import com.fasterxml.jackson.databind.type.TypeBindings;
+
+import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -10,6 +22,8 @@ import java.io.Serializable;
  *
  * @author livk
  */
+@JsonSerialize(using = Pair.PairJsonSerializer.class)
+@JsonDeserialize(using = Pair.PairJsonDeserializer.class)
 public record Pair<K, V>(K key, V value) implements Serializable, Cloneable {
 
     public static final Pair<?, ?> EMPTY = Pair.of(null, null);
@@ -27,6 +41,58 @@ public record Pair<K, V>(K key, V value) implements Serializable, Cloneable {
             return (Pair<K, V>) super.clone();
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
+        }
+    }
+
+    static class PairJsonSerializer extends StdScalarSerializer<Pair<Object, Object>> implements ContextualSerializer {
+
+        protected PairJsonSerializer() {
+            super(Pair.class, false);
+        }
+
+        private JsonSerializer<Object> keySerializer;
+
+        @Override
+        public void serialize(Pair<Object, Object> pair, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            gen.writeStartObject(pair);
+            keySerializer.serialize(pair.key(), gen, provider);
+            gen.writeObject(pair.value);
+            gen.writeEndObject();
+        }
+
+        @Override
+        public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
+            keySerializer = prov.findKeySerializer(Object.class, property);
+            return this;
+        }
+    }
+
+    static class PairJsonDeserializer extends StdScalarDeserializer<Pair<Object, Object>> implements ContextualDeserializer {
+
+        private KeyDeserializer keyDeserializer;
+        private JsonDeserializer<Object> valueDeserializer;
+
+        protected PairJsonDeserializer() {
+            super(Pair.class);
+        }
+
+        @Override
+        public Pair<Object, Object> deserialize(JsonParser p, DeserializationContext context) throws IOException {
+            p.nextToken();
+            String name = p.currentName();
+            p.nextToken();
+            return Pair.of(keyDeserializer.deserializeKey(name, context), valueDeserializer.deserialize(p, context));
+        }
+
+        @Override
+        public JsonDeserializer<?> createContextual(DeserializationContext context, BeanProperty property) throws JsonMappingException {
+            JavaType contextualType = context.getContextualType();
+            TypeBindings bindings = contextualType.getBindings();
+            JavaType keyType = bindings.getBoundType(0);
+            JavaType valueType = bindings.getBoundType(1);
+            keyDeserializer = context.findKeyDeserializer(keyType, property);
+            valueDeserializer = context.findContextualValueDeserializer(valueType, property);
+            return this;
         }
     }
 }
---
+++ spring-commons/src/test/java/com/livk/commons/domain/PairTest.java
@@ -0,0 +1,67 @@
+package com.livk.commons.domain;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.livk.commons.util.JacksonUtils;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * <p>
+ * PairTest
+ * </p>
+ *
+ * @author livk
+ * @date 2022/12/28
+ */
+class PairTest {
+
+    static Pair<String, Integer> pair = Pair.of("livk", 123456);
+
+    @Test
+    public void pairJsonSerializerTest() {
+        String json = "{\"livk\":123456}";
+        String result = JacksonUtils.toJsonStr(pair);
+        assertEquals(json, result);
+    }
+
+    @Test
+    public void pairJsonDeserializerTest() {
+        @Language("JSON") String json = "{\"livk\":123456}";
+        Pair<String, Integer> result = JacksonUtils.toBean(json, new TypeReference<>() {
+        });
+        assertEquals("livk", result.key());
+        assertEquals(123456, result.value());
+
+        @Language("JSON") String json2 = """
+                {"livk": {"root": "username"}}""";
+        Pair<String, Pair<String, String>> result2 = JacksonUtils.toBean(json2, new TypeReference<>() {
+        });
+        assertEquals("livk", result2.key());
+        assertEquals("root", result2.value().key());
+        assertEquals("username", result2.value().value());
+
+        @Language("JSON") String json3 = """
+                {"livk":  [1,2,3]}""";
+        Pair<String, List<Integer>> result3 = JacksonUtils.toBean(json3, new TypeReference<>() {
+        });
+        assertEquals("livk", result3.key());
+        assertEquals(List.of(1, 2, 3), result3.value());
+
+        @Language("JSON") String json4 = """
+                {
+                  "livk": {
+                    "username": "root",
+                    "password": "root"
+                  }
+                }""";
+        Pair<String, Map<String, String>> result4 = JacksonUtils.toBean(json4, new TypeReference<>() {
+        });
+        assertEquals("livk", result4.key());
+        assertEquals(Map.of("username", "root", "password", "root"), result4.value());
+    }
+}
