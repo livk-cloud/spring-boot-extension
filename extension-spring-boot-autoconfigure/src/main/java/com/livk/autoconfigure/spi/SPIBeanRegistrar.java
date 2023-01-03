7e61ff18e7c77216d5514ca9dbae7376a13fc674
livk <1375632510@qq.com>
2023-01-03 11:47:40 +0800

    refactor: SPI注入调整，跳过已注册

--- extension-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/spi/SPIBeanRegistrar.java
+++ extension-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/spi/SPIBeanRegistrar.java
@@ -3,8 +3,10 @@ package com.livk.autoconfigure.spi;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
@@ -26,16 +28,23 @@ public class SPIBeanRegistrar implements ImportBeanDefinitionRegistrar {
         AnnotationAttributes attributes = AnnotationAttributes
                 .fromMap(importingClassMetadata.getAnnotationAttributes(ImportSPI.class.getName()));
         if (!CollectionUtils.isEmpty(attributes)) {
-            Class<?> clazz = attributes.getClass("value");
-            Map<? extends Class<?>, ?> map = ServiceLoader.load(clazz)
+            Class<?> importSPIClass = attributes.getClass("value");
+            Map<Class<?>, ?> map = ServiceLoader.load(importSPIClass)
                     .stream()
                     .collect(Collectors.toMap(ServiceLoader.Provider::type,
                             ServiceLoader.Provider::get));
-            for (final Map.Entry<? extends Class<?>, ?> entry : map.entrySet()) {
-                RootBeanDefinition beanDefinition = new RootBeanDefinition(entry.getKey());
-                beanDefinition.setInstanceSupplier(entry::getValue);
-                String beanName = StringUtils.uncapitalize(entry.getKey().getSimpleName());
-                registry.registerBeanDefinition(beanName, beanDefinition);
+            for (Map.Entry<? extends Class<?>, ?> entry : map.entrySet()) {
+                Class<?> instanceClass = entry.getKey();
+                //如果当前类已经注册，则跳过
+                if (!AnnotatedElementUtils.hasAnnotation(instanceClass, Component.class)) {
+                    RootBeanDefinition beanDefinition = new RootBeanDefinition(entry.getKey());
+                    beanDefinition.setInstanceSupplier(entry::getValue);
+                    String beanName = StringUtils.uncapitalize(entry.getKey().getSimpleName());
+                    //如果当前beanName已经被注册，则跳过
+                    if (!registry.containsBeanDefinition(beanName)) {
+                        registry.registerBeanDefinition(beanName, beanDefinition);
+                    }
+                }
             }
         }
     }
