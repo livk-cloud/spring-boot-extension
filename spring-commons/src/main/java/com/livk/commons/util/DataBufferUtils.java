80fdb9de43a2cfc9d1bf1e66aab22c0bad90d7af
livk <1375632510@qq.com>
2023-01-05 16:02:28 +0800

    feat: DataBuffer转换

---
+++ spring-commons/src/main/java/com/livk/commons/util/DataBufferUtils.java
@@ -0,0 +1,44 @@
+package com.livk.commons.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.util.StreamUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+/**
+ * <p>
+ * DataBufferUtils
+ * </p>
+ *
+ * @author livk
+ * @date 2023/1/5
+ */
+@UtilityClass
+public class DataBufferUtils extends org.springframework.core.io.buffer.DataBufferUtils {
+
+    public InputStream transform(DataBuffer dataBuffer) {
+        byte[] array = dataBuffer.toByteBuffer().array();
+        return new ByteArrayInputStream(array);
+    }
+
+    public Mono<InputStream> transform(Part part) {
+        return join(part.content()).map(DataBufferUtils::transform);
+    }
+
+    public Flux<DataBuffer> transform(ByteArrayOutputStream byteArrayOutputStream) {
+        return transform(byteArrayOutputStream.toByteArray());
+    }
+
+    public Flux<DataBuffer> transform(byte[] array) {
+        ByteArrayResource resource = new ByteArrayResource(array);
+        return read(resource, DefaultDataBufferFactory.sharedInstance, StreamUtils.BUFFER_SIZE);
+    }
+}
