2dbda3054ab623d98e39d88888e5761b964e9eb6
livk <1375632510@qq.com>
2023-02-01 16:27:44 +0800

    refactor: PageHelperBannerSettings优化

--- spring-commons/src/main/java/com/livk/commons/spring/PageHelperBannerSettings.java
+++ spring-commons/src/main/java/com/livk/commons/spring/PageHelperBannerSettings.java
@@ -1,10 +1,11 @@
 package com.livk.commons.spring;
 
 import com.livk.auto.service.annotation.SpringFactories;
+import com.livk.commons.util.ClassUtils;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * <p>
@@ -14,18 +15,16 @@ import org.springframework.core.env.Environment;
  * @author livk
  */
 @AutoConfiguration
-@SpringFactories(type = ApplicationContextInitializer.class)
-public class PageHelperBannerSettings implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+@SpringFactories(type = EnvironmentPostProcessor.class)
+public class PageHelperBannerSettings implements EnvironmentPostProcessor {
 
     private static final String PAGEHELPER_BANNER = "pagehelper.banner";
 
     @Override
-    public void initialize(ConfigurableApplicationContext applicationContext) {
-        Environment environment = applicationContext.getEnvironment();
-
-        boolean PageHelperBannerEnable = Boolean
-                .parseBoolean(System.getProperty(PAGEHELPER_BANNER, environment.getProperty(PAGEHELPER_BANNER)));
-        System.setProperty(PAGEHELPER_BANNER, Boolean.toString(PageHelperBannerEnable));
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        if (ClassUtils.isPresent("com.github.pagehelper.PageInterceptor", Thread.currentThread().getContextClassLoader())) {
+            Boolean pageHelperBannerEnable = environment.getProperty(PAGEHELPER_BANNER, Boolean.class, false);
+            System.setProperty(PAGEHELPER_BANNER, pageHelperBannerEnable.toString());
+        }
     }
-
 }
