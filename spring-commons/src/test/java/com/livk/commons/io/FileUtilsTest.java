3bf668a377403ae06e19f56c6921c9135f5153a6
livk <1375632510@qq.com>
2023-02-02 09:59:52 +0800

    feat: FileUtils测试用例

---
+++ spring-commons/src/test/java/com/livk/commons/io/FileUtilsTest.java
@@ -0,0 +1,53 @@
+package com.livk.commons.io;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.livk.commons.jackson.JacksonUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * <p>
+ * FileUtilsTest
+ * </p>
+ *
+ * @author livk
+ * @date 2023/2/2
+ */
+class FileUtilsTest {
+
+    @Test
+    void download() throws IOException {
+        InputStream inputStream = new ByteArrayInputStream("livk".getBytes());
+        FileUtils.download(inputStream, "username.txt");
+        assertTrue(new File("username.txt").delete());
+    }
+
+    @Test
+    void createNewFile() throws IOException {
+        File file = new File("file.txt");
+        assertTrue(FileUtils.createNewFile(file));
+        assertTrue(file.delete());
+    }
+
+    @Test
+    void gzip() throws IOException {
+        InputStream inputStream = new ClassPathResource("data.json").getInputStream();
+        String data = JacksonUtils.toBean(inputStream, JsonNode.class).toString();
+        File file = new File("./data.gzip");
+        FileUtils.createNewFile(file);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            FileUtils.gzipCompress(data.getBytes(), fileOutputStream);
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            String copyData = new String(FileUtils.gzipDecompress(fileInputStream));
+            assertEquals(data, copyData);
+            assertTrue(file.delete());
+        }
+    }
+}
