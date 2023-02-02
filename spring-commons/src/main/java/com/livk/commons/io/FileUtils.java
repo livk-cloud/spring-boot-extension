aa5f39a6856f9210b9b214e52ae3613c401c22c3
livk <1375632510@qq.com>
2023-02-02 09:59:31 +0800

    feat: gzip压缩和解压

--- spring-commons/src/main/java/com/livk/commons/io/FileUtils.java
+++ spring-commons/src/main/java/com/livk/commons/io/FileUtils.java
@@ -1,19 +1,19 @@
 package com.livk.commons.io;
 
+import com.livk.commons.util.ObjectUtils;
 import lombok.experimental.UtilityClass;
 import org.springframework.http.codec.multipart.Part;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * The type File utils.
@@ -72,5 +72,36 @@ public class FileUtils extends FileCopyUtils {
     public Mono<Part> getPartValues(String name, ServerWebExchange exchange) {
         return exchange.getMultipartData()
                 .mapNotNull(multiValueMap -> multiValueMap.getFirst(name));
+    }
+
+
+    /**
+     * Gzip compress.
+     *
+     * @param bytes        the bytes
+     * @param outputStream the output stream
+     */
+    public static void gzipCompress(byte[] bytes, OutputStream outputStream) {
+        if (!ObjectUtils.isEmpty(bytes)) {
+            try (GZIPOutputStream stream = new GZIPOutputStream(outputStream)) {
+                stream.write(bytes);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Gzip decompress byte [ ].
+     *
+     * @param inputStream the input stream
+     * @return the byte [ ]
+     */
+    public static byte[] gzipDecompress(InputStream inputStream) {
+        try (GZIPInputStream stream = new GZIPInputStream(inputStream)) {
+            return FileUtils.copyToByteArray(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
