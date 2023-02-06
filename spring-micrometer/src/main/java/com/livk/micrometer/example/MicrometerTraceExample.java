f2db73ced1ad53e83254151225fa4d4eca4b60a6
livk <1375632510@qq.com>
2023-02-06 15:26:15 +0800

    docs: javadoc

--- spring-micrometer/src/main/java/com/livk/micrometer/example/MicrometerTraceExample.java
+++ spring-micrometer/src/main/java/com/livk/micrometer/example/MicrometerTraceExample.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- * MicrometerTraceExample
+ * {@link com.livk.commons.spring.TraceEnvironmentPostProcessor}
  * </p>
  *
  * @author livk
