374e68741abd841ddd97d979f366d9dc378cde9c
livk <1375632510@qq.com>
2023-02-10 17:08:57 +0800

    refactor: 基础类型使用simpleType判定

--- spring-commons/src/main/java/com/livk/commons/jackson/support/NumberJsonSerializer.java
+++ spring-commons/src/main/java/com/livk/commons/jackson/support/NumberJsonSerializer.java
@@ -1,11 +1,9 @@
 package com.livk.commons.jackson.support;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.BeanProperty;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import com.fasterxml.jackson.databind.type.SimpleType;
 import com.livk.commons.jackson.support.annotation.NumberJsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -13,7 +11,6 @@ import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.Optional;
 
 /**
@@ -33,16 +30,16 @@ public class NumberJsonSerializer extends JsonSerializer<Number> implements Cont
 
     @Override
     public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
-        Class<?> rawClass = property.getType().getRawClass();
+        JavaType javaType = property.getType();
         NumberJsonFormat jsonFormat = Optional.ofNullable(property.getAnnotation((NumberJsonFormat.class)))
                 .orElse(property.getContextAnnotation(NumberJsonFormat.class));
-        if (Number.class.isAssignableFrom(rawClass) || this.simpleTypeSupport(jsonFormat.simpleTypeSupport(), rawClass)) {
+        if (Number.class.isAssignableFrom(javaType.getRawClass()) || this.simpleTypeSupport(jsonFormat.simpleTypeSupport(), javaType)) {
             return new NumberJsonSerializer(jsonFormat.pattern());
         }
         return prov.findValueSerializer(property.getType(), property);
     }
 
-    private boolean simpleTypeSupport(boolean support, Class<?> rawClass) {
-        return support && Arrays.asList(Double.TYPE, Float.TYPE, Long.TYPE).contains(rawClass);
+    private boolean simpleTypeSupport(boolean support, JavaType javaType) {
+        return support && javaType instanceof SimpleType;
     }
 }
