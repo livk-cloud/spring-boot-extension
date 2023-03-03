2e996ce528480156b6b2e28f5b19acb260ea212e
livk <1375632510@qq.com>
2023-03-03 13:58:52 +0800

    refactor: oss

--- extension-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/oss/support/AbstractService.java
+++ extension-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/oss/support/AbstractService.java
@@ -3,10 +3,23 @@ package com.livk.autoconfigure.oss.support;
 import com.livk.autoconfigure.oss.OSSProperties;
 import com.livk.autoconfigure.oss.client.OSSClientFactory;
 
+/**
+ * The type Abstract service.
+ *
+ * @param <T> the type parameter
+ */
 public abstract non-sealed class AbstractService<T> implements OSSOperations {
 
-    protected T client;
+    /**
+     * The Client.
+     */
+    protected final T client;
 
+    /**
+     * Instantiates a new Abstract service.
+     *
+     * @param properties the properties
+     */
     protected AbstractService(OSSProperties properties) {
         OSSClientFactoryPatternResolver resolver = new OSSClientFactoryPatternResolver();
         OSSClientFactory<T> factory = resolver.loader(properties.getPrefix());
