f5022b76f7ad10ff9f577d433a952aa2c84c7460
livk <1375632510@qq.com>
2023-03-06 16:13:20 +0800

    docs: javadoc

--- spring-commons/src/main/java/com/livk/commons/spi/AbstractLoader.java
+++ spring-commons/src/main/java/com/livk/commons/spi/AbstractLoader.java
@@ -3,6 +3,8 @@ package com.livk.commons.spi;
 import java.util.List;
 
 /**
+ * The type Abstract loader.
+ *
  * @author livk
  */
 public abstract class AbstractLoader implements Loader {
@@ -12,5 +14,13 @@ public abstract class AbstractLoader implements Loader {
         return load(type, classLoader);
     }
 
+    /**
+     * Load list.
+     *
+     * @param <T>         the type parameter
+     * @param type        the type
+     * @param classLoader the class loader
+     * @return the list
+     */
     protected abstract <T> List<T> load(Class<T> type, ClassLoader classLoader);
 }
