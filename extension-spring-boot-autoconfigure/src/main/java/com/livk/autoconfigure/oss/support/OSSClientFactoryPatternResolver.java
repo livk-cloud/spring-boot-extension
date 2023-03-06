d10ed7b6b2534135b4e9fe057230e99897cfe9f0
livk <1375632510@qq.com>
2023-03-06 14:55:00 +0800

    refactor: oss

--- extension-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/oss/support/OSSClientFactoryPatternResolver.java
+++ extension-spring-boot-autoconfigure/src/main/java/com/livk/autoconfigure/oss/support/OSSClientFactoryPatternResolver.java
@@ -2,7 +2,10 @@ package com.livk.autoconfigure.oss.support;
 
 import com.livk.autoconfigure.oss.client.OSSClientFactory;
 import com.livk.autoconfigure.oss.client.OSSClientFactoryLoader;
+import com.livk.autoconfigure.oss.exception.OSSClientFactoryNotFoundException;
 import org.springframework.core.io.support.SpringFactoriesLoader;
+
+import java.util.List;
 
 /**
  * The type Oss client factory pattern resolver.
@@ -14,11 +17,13 @@ class OSSClientFactoryPatternResolver implements OSSClientFactoryLoader {
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
     public <T> OSSClientFactory<T> loader(String prefix) {
-        for (OSSClientFactory factory : SpringFactoriesLoader.loadFactories(OSSClientFactory.class, this.getClass().getClassLoader())) {
+        List<OSSClientFactory> factories = SpringFactoriesLoader.loadFactories(OSSClientFactory.class, this.getClass().getClassLoader());
+        for (OSSClientFactory factory : factories) {
             if (factory.prefix().equals(prefix)) {
                 return (OSSClientFactory<T>) factory;
             }
         }
-        throw new RuntimeException("oss factory匹配失败");
+        List<String> prefixList = factories.stream().map(OSSClientFactory::prefix).toList();
+        throw new OSSClientFactoryNotFoundException(prefix + " oss factory匹配失败,当前可用oss factory :" + prefixList);
     }
 }
