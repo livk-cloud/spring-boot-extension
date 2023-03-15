7d880ed0c531f7a05423b397a610a18ad0d0062b
livk <1375632510@qq.com>
2023-03-15 21:26:56 +0800

    fix: test

--- example/spring-security/security-sso/sso-resources/src/test/java/com/livk/sso/resource/controller/UserControllerTest.java
+++ example/spring-security/security-sso/sso-resources/src/test/java/com/livk/sso/resource/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.livk.sso.resource.controller;
 
 import com.livk.commons.jackson.JacksonUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -27,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *
  * @author livk
  */
-//@Disabled("需要启动授权服务器")
+@Disabled("需要启动授权服务器")
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
