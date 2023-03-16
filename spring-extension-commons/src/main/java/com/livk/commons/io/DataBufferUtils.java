e1899268ce28a2637bcc45ac4986a5b7154f855a
livk <1375632510@qq.com>
2023-03-16 14:52:25 +0800

    refactor: DataBufferUtils

--- spring-extension-commons/src/main/java/com/livk/commons/io/DataBufferUtils.java
+++ spring-extension-commons/src/main/java/com/livk/commons/io/DataBufferUtils.java
@@ -3,11 +3,14 @@ package com.livk.commons.io;
 import lombok.experimental.UtilityClass;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.util.StreamUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -19,6 +22,17 @@ import java.io.InputStream;
  */
 @UtilityClass
 public class DataBufferUtils extends org.springframework.core.io.buffer.DataBufferUtils {
+
+    /**
+     * The constant BUFFER_SIZE.
+     */
+    public static final int BUFFER_SIZE = StreamUtils.BUFFER_SIZE;
+
+    /**
+     * The constant DEFAULT_FACTORY.
+     */
+    public static final DataBufferFactory DEFAULT_FACTORY = DefaultDataBufferFactory.sharedInstance;
+
     /**
      * Transform mono.
      *
@@ -37,6 +51,44 @@ public class DataBufferUtils extends org.springframework.core.io.buffer.DataBuff
      */
     public Flux<DataBuffer> transform(byte[] array) {
         ByteArrayResource resource = new ByteArrayResource(array);
-        return read(resource, DefaultDataBufferFactory.sharedInstance, StreamUtils.BUFFER_SIZE);
+        return read(resource, DEFAULT_FACTORY, BUFFER_SIZE);
+    }
+
+    /**
+     * Transform byte mono.
+     *
+     * @param bufferFlux the buffer flux
+     * @return the mono
+     */
+    public Mono<byte[]> transformByte(Flux<DataBuffer> bufferFlux) {
+        return DataBufferUtils.transform(bufferFlux)
+                .publishOn(Schedulers.boundedElastic())
+                .map(inputStream -> {
+                    try {
+                        return inputStream.readAllBytes();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    /**
+     * Transform flux.
+     *
+     * @param inputStream the input stream
+     * @return the flux
+     */
+    public Flux<DataBuffer> transform(InputStream inputStream) {
+        return readInputStream(() -> inputStream, DEFAULT_FACTORY, BUFFER_SIZE);
+    }
+
+    /**
+     * Transform flux.
+     *
+     * @param inputStreamMono the input stream mono
+     * @return the flux
+     */
+    public Flux<DataBuffer> transform(Mono<InputStream> inputStreamMono) {
+        return inputStreamMono.flatMapMany(DataBufferUtils::transform);
     }
 }
--- spring-extension-commons/src/test/java/com/livk/commons/io/DataBufferUtilsTest.java
+++ spring-extension-commons/src/test/java/com/livk/commons/io/DataBufferUtilsTest.java
@@ -1,10 +1,12 @@
 package com.livk.commons.io;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.livk.commons.jackson.JacksonUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.buffer.DataBuffer;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
@@ -15,20 +17,24 @@ import java.io.IOException;
 class DataBufferUtilsTest {
 
     @Test
-    void transform() {
+    void transform() throws IOException {
         String text = "123456";
-        Flux<DataBuffer> bufferFlux = DataBufferUtils.transform(text.getBytes());
-        Mono<String> mono = DataBufferUtils.transform(bufferFlux)
-                .publishOn(Schedulers.boundedElastic())
-                .map(inputStream -> {
-                    try {
-                        return new String(inputStream.readAllBytes());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-        StepVerifier.create(mono)
+        StepVerifier.create(toMonoStr(DataBufferUtils.transform(text.getBytes())))
                 .expectNext(text)
                 .verifyComplete();
+
+        ClassPathResource resource = new ClassPathResource("input.json");
+        StepVerifier.create(toMonoStr(DataBufferUtils.transform(resource.getInputStream())).map(JacksonUtils::readTree))
+                .expectNext(JacksonUtils.readValue(resource.getInputStream(), JsonNode.class))
+                .verifyComplete();
+
+        StepVerifier.create(toMonoStr(DataBufferUtils.transform(Mono.just(resource.getInputStream()))).map(JacksonUtils::readTree))
+                .expectNext(JacksonUtils.readValue(resource.getInputStream(), JsonNode.class))
+                .verifyComplete();
+    }
+
+    private Mono<String> toMonoStr(Flux<DataBuffer> bufferFlux) {
+        return DataBufferUtils.transformByte(bufferFlux)
+                .map(String::new);
     }
 }
