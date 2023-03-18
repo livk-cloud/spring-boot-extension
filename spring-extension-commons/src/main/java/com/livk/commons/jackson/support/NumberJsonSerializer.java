f0ac882207ad8959d7f582c74818e0118edf695b
livk <1375632510@qq.com>
2023-03-18 14:58:53 +0800

    refactor: 基础类型限制

--- spring-extension-commons/src/main/java/com/livk/commons/jackson/support/NumberJsonSerializer.java
+++ spring-extension-commons/src/main/java/com/livk/commons/jackson/support/NumberJsonSerializer.java
@@ -1,23 +1,32 @@
 package com.livk.commons.jackson.support;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
-import com.fasterxml.jackson.databind.type.SimpleType;
 import com.livk.commons.jackson.annotation.NumberJsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Optional;
 
 /**
+ * The type Number json serializer.
+ *
  * @author livk
  */
 @AllArgsConstructor
 @NoArgsConstructor
 public class NumberJsonSerializer extends JsonSerializer<Number> implements ContextualSerializer {
+
+    private static final List<Class<?>> SUPPORT_PRIMITIVE_CLASS = List.of(
+            byte.class, short.class, int.class, long.class,
+            float.class, double.class);
 
     private String format;
 
@@ -28,16 +37,16 @@ public class NumberJsonSerializer extends JsonSerializer<Number> implements Cont
 
     @Override
     public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
-        JavaType javaType = property.getType();
+        Class<?> rawClass = property.getType().getRawClass();
         NumberJsonFormat jsonFormat = Optional.ofNullable(property.getAnnotation((NumberJsonFormat.class)))
                 .orElse(property.getContextAnnotation(NumberJsonFormat.class));
-        if (Number.class.isAssignableFrom(javaType.getRawClass()) || this.simpleTypeSupport(jsonFormat.simpleTypeSupport(), javaType)) {
+        if (Number.class.isAssignableFrom(rawClass) || this.simpleTypeSupport(jsonFormat.simpleTypeSupport(), rawClass)) {
             return new NumberJsonSerializer(jsonFormat.pattern());
         }
         return prov.findValueSerializer(property.getType(), property);
     }
 
-    private boolean simpleTypeSupport(boolean support, JavaType javaType) {
-        return support && javaType instanceof SimpleType;
+    private boolean simpleTypeSupport(boolean support, Class<?> rawClass) {
+        return support && rawClass.isPrimitive() && SUPPORT_PRIMITIVE_CLASS.contains(rawClass);
     }
 }
