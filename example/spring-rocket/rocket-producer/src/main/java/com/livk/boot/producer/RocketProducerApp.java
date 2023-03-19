923e183a841a68c3408b14936b715d9bb76b6d45
livk <1375632510@qq.com>
2023-03-19 12:51:50 +0800

    refactor: 适配springboot3自动装配

--- example/spring-rocket/rocket-producer/src/main/java/com/livk/boot/producer/RocketProducerApp.java
+++ example/spring-rocket/rocket-producer/src/main/java/com/livk/boot/producer/RocketProducerApp.java
@@ -1,14 +1,12 @@
 package com.livk.boot.producer;
+
 import com.livk.commons.spring.SpringLauncher;
-import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author laokou
  */
 @SpringBootApplication
-@Import({RocketMQAutoConfiguration.class})
 public class RocketProducerApp {
 
     public static void main(String[] args) {
