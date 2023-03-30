cb9409ca2bea57df9f7759009f30f0438ab16609
livk <1375632510@qq.com>
2023-03-31 00:23:04 +0800

    refactor: SpringHttpTest

--- spring-extension-commons/src/test/java/com/livk/commons/http/SpringHttpTest.java
+++ spring-extension-commons/src/test/java/com/livk/commons/http/SpringHttpTest.java
@@ -2,12 +2,14 @@ package com.livk.commons.http;
 
 import com.livk.commons.http.annotation.EnableHttpClient;
 import com.livk.commons.http.annotation.EnableWebClient;
+import com.livk.commons.spring.context.SpringContextHolder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
@@ -28,5 +30,7 @@ public class SpringHttpTest {
     public void test() {
         assertNotNull(restTemplate);
         assertNotNull(webClient);
+        assertEquals(SpringContextHolder.getBean(RestTemplate.class), restTemplate);
+        assertEquals(SpringContextHolder.getBean(WebClient.class), webClient);
     }
 }
