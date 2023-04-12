0d9a55d7f7d1a545e501a570716ea754caee2d26
livk <1375632510@qq.com>
2023-04-12 12:40:31 +0800

    refactor: CacheInterceptor切换SpEL

--- example/spring-redis/redis-caffeine/src/main/java/com/livk/caffeine/aspect/CacheInterceptor.java
+++ example/spring-redis/redis-caffeine/src/main/java/com/livk/caffeine/aspect/CacheInterceptor.java
@@ -3,7 +3,7 @@ package com.livk.caffeine.aspect;
 import com.livk.caffeine.annotation.DoubleCache;
 import com.livk.caffeine.handler.CacheHandlerAdapter;
 import com.livk.commons.aop.AnnotationAbstractPointcutTypeAdvisor;
-import com.livk.commons.spring.util.SpringUtils;
+import com.livk.commons.spring.spel.SpringExpressionResolver;
 import lombok.RequiredArgsConstructor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.stereotype.Component;
@@ -22,10 +22,12 @@ public class CacheInterceptor extends AnnotationAbstractPointcutTypeAdvisor<Doub
 
     private final CacheHandlerAdapter adapter;
 
+    private final SpringExpressionResolver resolver = new SpringExpressionResolver();
+
     @Override
     protected Object invoke(MethodInvocation invocation, DoubleCache doubleCache) throws Throwable {
         Assert.notNull(doubleCache, "doubleCache is null");
-        String spELResult = SpringUtils.parseSpEL(invocation.getMethod(), invocation.getArguments(), doubleCache.key());
+        String spELResult = resolver.evaluate(doubleCache.key(), invocation.getMethod(), invocation.getArguments());
         String realKey = doubleCache.cacheName() + ":" + spELResult;
         switch (doubleCache.type()) {
             case FULL -> {
