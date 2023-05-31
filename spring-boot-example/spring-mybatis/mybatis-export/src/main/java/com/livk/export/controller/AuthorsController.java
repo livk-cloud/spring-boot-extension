410e8b73db0d320dc7d2d048615471ab94d24ad8
livk <1375632510@qq.com>
2023-05-31 21:18:08 +0800

    refactor: 使用HttpHeaders

--- spring-boot-example/spring-mybatis/mybatis-export/src/main/java/com/livk/export/controller/AuthorsController.java
+++ spring-boot-example/spring-mybatis/mybatis-export/src/main/java/com/livk/export/controller/AuthorsController.java
@@ -21,6 +21,7 @@ import com.livk.export.service.AuthorsService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,8 +41,8 @@ public class AuthorsController {
     @GetMapping("download")
     public HttpEntity<Void> download(HttpServletResponse response) throws IOException {
         String fileName = System.currentTimeMillis() + ".csv";
-        response.addHeader("Content-Type", "application/csv");
-        response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+        response.addHeader(HttpHeaders.CONTENT_TYPE, "application/csv");
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
         response.setCharacterEncoding("UTF-8");
         try (PrintWriter writer = response.getWriter()) {
             authorsService.download(writer);
