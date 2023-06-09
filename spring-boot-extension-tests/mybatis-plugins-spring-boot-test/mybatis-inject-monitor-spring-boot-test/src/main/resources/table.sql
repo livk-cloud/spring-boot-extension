89ab0755fd8284b32b2ca29e83b0afa4ba0524d2
livk <1375632510@qq.com>
2023-06-09 18:06:08 +0800

    refactor: SQL格式化

--- spring-boot-extension-tests/mybatis-plugins-spring-boot-test/mybatis-inject-monitor-spring-boot-test/src/main/resources/table.sql
+++ spring-boot-extension-tests/mybatis-plugins-spring-boot-test/mybatis-inject-monitor-spring-boot-test/src/main/resources/table.sql
@@ -15,24 +15,11 @@
  *
  */
 
-create table if not exists ` user `
+create table if not exists `user`
 (
-    `
-    id
-    `
-    int
-    not
-    null
-    primary
-    key,
-    `
-    username
-    `
-    varchar
-(
-    20
-),
-    ` version ` int,
-    ` insert_time ` datetime,
-    ` update_time ` datetime
-    );
+    id          int not null primary key,
+    username    varchar(20),
+    version     int,
+    insert_time datetime,
+    update_time datetime
+);
