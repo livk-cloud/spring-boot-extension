e324b83e92a41528a69b6fa4dcb9e00e56adbb22
livk <1375632510@qq.com>
2023-06-27 13:44:07 +0800

    test: kafka-protobuf

--- spring-boot-example/spring-protobuf-mq/spring-protobuf-kafka/src/test/java/com/livk/proto/kafka/controller/UserControllerTest.java
+++ spring-boot-example/spring-protobuf-mq/spring-protobuf-kafka/src/test/java/com/livk/proto/kafka/controller/UserControllerTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,7 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author livk
  */
-@SpringBootTest
+@SpringBootTest("spring.kafka.bootstrap-servers=localhost:9092")
+@EmbeddedKafka(ports = 9092)
 @AutoConfigureMockMvc
 class UserControllerTest {
 
