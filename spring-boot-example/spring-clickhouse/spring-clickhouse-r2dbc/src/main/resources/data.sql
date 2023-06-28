70732fdeae16f76cacc3449d710cdb19ed0119b8
livk <1375632510@qq.com>
2023-06-28 14:24:42 +0800

    ci: clickhouse 测试用例

--- spring-boot-example/spring-clickhouse/spring-clickhouse-r2dbc/src/main/resources/data.sql
+++ spring-boot-example/spring-clickhouse/spring-clickhouse-r2dbc/src/main/resources/data.sql
@@ -17,17 +17,10 @@
 
 create table if not exists user
 (
-    id
-        Int32,
-    app_id
-        String,
-    version
-        String,
-    reg_time
-        Date
-) engine = MergeTree PARTITION BY toYYYYMM
-(
-    reg_time
-)
-    ORDER BY id
-    SETTINGS index_granularity = 8192;
+    id       Int32,
+    app_id   String,
+    version  String,
+    reg_time Date
+) engine = MergeTree PARTITION BY toYYYYMM(reg_time)
+      ORDER BY id
+      SETTINGS index_granularity = 8192;
