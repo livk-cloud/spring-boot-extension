695fb4ca16af39ee05112dbfb150cae8d995f0b6
livk <1375632510@qq.com>
2023-07-14 09:54:03 +0800

    fix: 修复测试用例

--- spring-extension-commons/src/test/java/com/livk/commons/beans/WrapperTest.java
+++ spring-extension-commons/src/test/java/com/livk/commons/beans/WrapperTest.java
@@ -17,7 +17,6 @@
 
 package com.livk.commons.beans;
 
-import com.livk.commons.util.GenericWrapper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
