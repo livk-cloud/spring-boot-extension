be609fd3fe3afda6208424a47eb2a70920d0eb06
livk <1375632510@qq.com>
2023-10-10 23:09:16 +0800

    feat: RedisEventStore

--- spring-boot-example/spring-admin/spring-admin-server/build.gradle.kts
+++ spring-boot-example/spring-admin/spring-admin-server/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("de.codecentric:spring-boot-admin-starter-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
