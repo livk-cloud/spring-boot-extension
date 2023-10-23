fa636ae7f4151a7497196320a43ddd0eb574bf63
livk <1375632510@qq.com>
2023-10-23 14:49:02 +0800

    style: 格式等调整

--- spring-boot-example/spring-security/security-sso/sso-auth/src/main/java/com/livk/sso/auth/handler/CustomAuthenticationFailureHandler.java
+++ spring-boot-example/spring-security/security-sso/sso-auth/src/main/java/com/livk/sso/auth/handler/CustomAuthenticationFailureHandler.java
@@ -18,14 +18,12 @@
 package com.livk.sso.auth.handler;
 
 import com.livk.commons.util.WebUtils;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
-import java.io.IOException;
 import java.util.Map;
 
 /**
--- spring-boot-example/spring-security/security-sso/sso-auth/src/main/java/com/livk/sso/auth/support/CustomAuthenticationEntryPoint.java
+++ spring-boot-example/spring-security/security-sso/sso-auth/src/main/java/com/livk/sso/auth/support/CustomAuthenticationEntryPoint.java
@@ -18,14 +18,12 @@
 package com.livk.sso.auth.support;
 
 import com.livk.commons.util.WebUtils;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import java.io.IOException;
 import java.util.Map;
 
 /**
