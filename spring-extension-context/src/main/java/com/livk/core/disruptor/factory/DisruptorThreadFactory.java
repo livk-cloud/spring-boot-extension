7ffbd2070faee67f7597f0c55265963bccc0e1df
livk <livk.cloud@gmail.com>
2023-11-28 11:50:16 +0800

    refactor: 移除计数器

--- spring-extension-context/src/main/java/com/livk/core/disruptor/factory/DisruptorThreadFactory.java
+++ spring-extension-context/src/main/java/com/livk/core/disruptor/factory/DisruptorThreadFactory.java
@@ -20,18 +20,15 @@ package com.livk.core.disruptor.factory;
 import org.springframework.lang.NonNull;
 
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author livk
  */
 public class DisruptorThreadFactory implements ThreadFactory {
 
-	private static final AtomicLong index = new AtomicLong(1);
-
 	@Override
 	public Thread newThread(@NonNull Runnable r) {
-		return new Thread(r, "disruptor-" + index.getAndIncrement());
+		return new Thread(r, "disruptor");
 	}
 
 }
