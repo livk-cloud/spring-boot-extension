dfbfaefbe4b835585caabe47ad42403f7a09c626
livk <livk.cloud@gmail.com>
2024-05-11 14:50:39 +0800

    refactor: redisson test切换至mockmvc

--- spring-boot-extension-tests/redis-spring-boot-test/redisson-spring-boot-test/src/test/java/com/livk/redisson/schedule/ScheduleControllerTest.java
+++ spring-boot-extension-tests/redis-spring-boot-test/redisson-spring-boot-test/src/test/java/com/livk/redisson/schedule/ScheduleControllerTest.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2021-2024 spring-boot-extension the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  *       https://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,23 +16,31 @@ package com.livk.redisson.schedule;
 import com.livk.testcontainers.containers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * <p>
- * ScheduleHandlerTest
+ * ScheduleControllerTest
  * </p>
  *
  * @author livk
  */
 @SpringBootTest
+@AutoConfigureMockMvc
 @Testcontainers(disabledWithoutDocker = true)
-class ScheduleHandlerTest {
+class ScheduleControllerTest {
 
 	@Container
 	@ServiceConnection
@@ -48,11 +53,11 @@ class ScheduleHandlerTest {
 	}
 
 	@Autowired
-	ScheduleHandler scheduleHandler;
+	MockMvc mockMvc;
 
 	@Test
-	void start() {
-		scheduleHandler.start();
+	void handler() throws Exception {
+		mockMvc.perform(get("/schedule")).andDo(print()).andExpect(status().isOk()).andExpect(content().string("ok"));
 	}
 
 }
