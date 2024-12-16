1950645ce8b583babd38c565d40a3c14f7839dd5
livk <livk.cloud@gmail.com>
2024-12-16 11:38:16 +0800

    refactor: 优化OkHttp装配方式

---
+++ spring-extension-commons/src/main/java/com/livk/commons/http/OkHttpClientConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021-2024 spring-boot-extension the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.livk.commons.http;
+
+import com.livk.commons.http.support.OkHttpClientHttpRequestFactory;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Fallback;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>
+ * OkHttpClientConfiguration
+ * </p>
+ *
+ * @author livk
+ */
+@AutoConfiguration
+@ConditionalOnClass(OkHttpClient.class)
+class OkHttpClientConfiguration {
+
+	@Bean
+	@Fallback
+	@ConditionalOnMissingBean(OkHttpClient.class)
+	public OkHttpClientHttpRequestFactory defaultOkHttpClientHttpRequestFactory() {
+		ConnectionPool pool = new ConnectionPool(200, 300, TimeUnit.SECONDS);
+		return new OkHttpClientHttpRequestFactory().connectionPool(pool)
+			.connectTimeout(3, TimeUnit.SECONDS)
+			.readTimeout(3, TimeUnit.SECONDS)
+			.writeTimeout(3, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Rest template customizer rest template customizer.
+	 * @param okHttpClient the ok http client
+	 * @return the rest template customizer
+	 */
+	@Bean
+	@ConditionalOnBean(OkHttpClient.class)
+	public OkHttpClientHttpRequestFactory okHttpClientHttpRequestFactory(OkHttpClient okHttpClient) {
+		return new OkHttpClientHttpRequestFactory(okHttpClient);
+	}
+
+}
--- spring-extension-commons/src/main/java/com/livk/commons/http/RestTemplateConfiguration.java
+++ spring-extension-commons/src/main/java/com/livk/commons/http/RestTemplateConfiguration.java
@@ -18,21 +18,17 @@ package com.livk.commons.http;
 
 import com.livk.auto.service.annotation.SpringAutoService;
 import com.livk.commons.http.annotation.EnableRestTemplate;
-import com.livk.commons.http.support.OkHttpClientHttpRequestFactory;
-import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -43,6 +39,7 @@ import java.util.concurrent.TimeUnit;
  */
 @AutoConfiguration
 @SpringAutoService(EnableRestTemplate.class)
+@ImportAutoConfiguration(OkHttpClientConfiguration.class)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class RestTemplateConfiguration {
 
@@ -56,39 +53,10 @@ public class RestTemplateConfiguration {
 		return builder.build();
 	}
 
-	/**
-	 * The type Ok http client configuration.
-	 */
-	@Configuration(enforceUniqueMethods = false)
-	@ConditionalOnClass(OkHttpClient.class)
-	public static class OkHttpClientConfiguration {
-
-		/**
-		 * Rest template customizer rest template customizer.
-		 * @return the rest template customizer
-		 */
-		@Bean
-		@ConditionalOnMissingBean(OkHttpClient.class)
-		public RestTemplateCustomizer restTemplateCustomizer() {
-			ConnectionPool pool = new ConnectionPool(200, 300, TimeUnit.SECONDS);
-			OkHttpClientHttpRequestFactory requestFactory = new OkHttpClientHttpRequestFactory().connectionPool(pool)
-				.connectTimeout(3, TimeUnit.SECONDS)
-				.readTimeout(3, TimeUnit.SECONDS)
-				.writeTimeout(3, TimeUnit.SECONDS);
-			return restTemplate -> restTemplate.setRequestFactory(requestFactory);
-		}
-
-		/**
-		 * Rest template customizer rest template customizer.
-		 * @param okHttpClient the ok http client
-		 * @return the rest template customizer
-		 */
-		@Bean
-		@ConditionalOnBean(OkHttpClient.class)
-		public RestTemplateCustomizer restTemplateCustomizer(OkHttpClient okHttpClient) {
-			return restTemplate -> restTemplate.setRequestFactory(new OkHttpClientHttpRequestFactory(okHttpClient));
-		}
-
+	@Bean
+	@ConditionalOnBean(ClientHttpRequestFactory.class)
+	public RestTemplateCustomizer restTemplateCustomizer(ClientHttpRequestFactory requestFactory) {
+		return restTemplate -> restTemplate.setRequestFactory(requestFactory);
 	}
 
 }
