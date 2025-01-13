96f8e23025903024e7f6ceb2e52c55a409e53953
livk <livk.cloud@gmail.com>
2025-01-13 14:45:59 +0800

    test: ImportSelector添加测试

---
+++ spring-extension-commons/src/test/java/com/livk/commons/http/HttpClientImportSelectorTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021-2024 spring-boot-extension the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.livk.commons.http;
+
+import com.livk.commons.http.annotation.EnableHttpClient;
+import com.livk.commons.http.annotation.HttpClientType;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.type.AnnotationMetadata;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * @author livk
+ */
+class HttpClientImportSelectorTest {
+
+	final HttpClientImportSelector importSelector = new HttpClientImportSelector();
+
+	@Test
+	void test() {
+		{
+			String[] imports = importSelector.selectImports(AnnotationMetadata.introspect(AllConfig.class));
+			String[] result = new String[] { WebClientConfiguration.class.getName(),
+					RestClientConfiguration.class.getName(), RestTemplateConfiguration.class.getName() };
+			assertArrayEquals(result, imports);
+		}
+
+		{
+			String[] imports = importSelector.selectImports(AnnotationMetadata.introspect(TwoConfig.class));
+			String[] result = new String[] { WebClientConfiguration.class.getName(),
+					RestClientConfiguration.class.getName() };
+			assertArrayEquals(result, imports);
+		}
+
+		{
+			String[] imports = importSelector.selectImports(AnnotationMetadata.introspect(OneConfig.class));
+			String[] result = new String[] { WebClientConfiguration.class.getName() };
+			assertArrayEquals(result, imports);
+		}
+	}
+
+	@EnableHttpClient({ HttpClientType.WEB_CLIENT, HttpClientType.REST_CLIENT, HttpClientType.REST_TEMPLATE })
+	static class AllConfig {
+
+	}
+
+	@EnableHttpClient({ HttpClientType.WEB_CLIENT, HttpClientType.REST_CLIENT })
+	static class TwoConfig {
+
+	}
+
+	@EnableHttpClient({ HttpClientType.WEB_CLIENT })
+	static class OneConfig {
+
+	}
+
+}
--- spring-extension-commons/src/test/java/com/livk/commons/spring/AutoImportSelectorTest.java
+++ spring-extension-commons/src/test/java/com/livk/commons/spring/AutoImportSelectorTest.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2021-2024 spring-boot-extension the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  *       https://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,43 +23,34 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * <p>
- * SpringAbstractImportSelectorTest
- * </p>
- *
  * @author livk
  */
-class SpringAbstractImportSelectorTest {
+class AutoImportSelectorTest {
+
+	final AutoImportSelector importSelector = new AutoImportSelector();
 
 	@Test
-	void testFindAnnotation() {
-		MyAnnotationImportSelector selector = new MyAnnotationImportSelector();
-		assertEquals(MyAnnotation.class, selector.getAnnotationClass());
-
-		String[] imports = selector.selectImports(AnnotationMetadata.introspect(Config.class));
-		String[] result = new String[] { MyAnnotationConfig.class.getName() };
+	void test() {
+		String[] imports = importSelector.selectImports(AnnotationMetadata.introspect(Config.class));
+		String[] result = new String[] { ImportConfig.class.getName() };
 		assertArrayEquals(result, imports);
 	}
 
+	@AutoImport
 	@Target(ElementType.TYPE)
 	@Retention(RetentionPolicy.RUNTIME)
-	@interface MyAnnotation {
+	@interface AutoServiceImport {
 
 	}
 
-	static class MyAnnotationImportSelector extends SpringAbstractImportSelector<MyAnnotation> {
+	@SpringAutoService(AutoServiceImport.class)
+	static class ImportConfig {
 
 	}
 
-	@SpringAutoService(MyAnnotation.class)
-	static class MyAnnotationConfig {
-
-	}
-
-	@MyAnnotation
+	@AutoServiceImport
 	static class Config {
 
 	}
