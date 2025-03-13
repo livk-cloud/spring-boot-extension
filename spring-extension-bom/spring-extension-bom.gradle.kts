186ea8f9aa07d0f0a4343e66ba1cc5e3d38c4703
livk <livk.cloud@gmail.com>
2025-03-13 18:28:37 +0800

    refactor: 优化subprojects导入

--- spring-extension-bom/spring-extension-bom.gradle.kts
+++ spring-extension-bom/spring-extension-bom.gradle.kts
@@ -10,8 +10,10 @@ dependencies {
 		api(project(":spring-extension-commons"))
 		api(project(":spring-extension-context"))
 		api(project(":spring-boot-extension-autoconfigure"))
-		project.project(project(":spring-boot-extension-starters").path).subprojects {
-			api(this)
+		project(":spring-boot-extension-starters") {
+			subprojects{
+				api(this)
+			}
 		}
 	}
 }
