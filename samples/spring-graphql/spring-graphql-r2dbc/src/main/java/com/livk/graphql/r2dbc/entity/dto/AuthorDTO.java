d8c9bacdb2164c58752beee28d805d209a2d5e61
livk <livk.cloud@gmail.com>
2025-07-21 15:49:22 +0800

    feat: 移除SpringHeaderCheck

--- samples/spring-graphql/spring-graphql-r2dbc/src/main/java/com/livk/graphql/r2dbc/entity/dto/AuthorDTO.java
+++ samples/spring-graphql/spring-graphql-r2dbc/src/main/java/com/livk/graphql/r2dbc/entity/dto/AuthorDTO.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * you may obtain a copy of the License at
+ * You may obtain a copy of the License at
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.livk.graphql.r2dbc.entity.dto;
