231af74afc3285bbf7dd6d5759536d7964815129
livk <livk.cloud@gmail.com>
2025-07-21 09:36:55 +0800

    feat: 移除crypto示例

--- samples/spring-crypto/spring-crypto-webflux/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-	com.livk.service
-}
-
-dependencies {
-	implementation(project(":samples:spring-crypto:crypto-commons"))
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-}
