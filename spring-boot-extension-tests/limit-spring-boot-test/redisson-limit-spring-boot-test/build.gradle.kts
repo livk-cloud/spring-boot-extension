8a3cf0dbb103faa69ea819f299d0ee07522fbbd4
livk <livk.cloud@gmail.com>
2025-12-01 17:32:00 +0800

    fix: 处理启动报错问题

--- spring-boot-extension-tests/limit-spring-boot-test/redisson-limit-spring-boot-test/build.gradle.kts
+++ spring-boot-extension-tests/limit-spring-boot-test/redisson-limit-spring-boot-test/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation(project(":spring-boot-extension-starters:limit-spring-boot-starter"))
+	implementation("org.springframework.boot:spring-boot-data-redis")
 
 	testImplementation(project(":spring-testcontainers-support"))
 	testImplementation("com.redis:testcontainers-redis")
-	testImplementation("org.springframework.boot:spring-boot-data-redis")
 	testImplementation("org.springframework.boot:spring-boot-webmvc-test")
 }
--- spring-boot-extension-tests/redisson-spring-boot-test/build.gradle.kts
+++ spring-boot-extension-tests/redisson-spring-boot-test/build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-data-redis")
 	implementation(project(":spring-boot-extension-starters:redisson-spring-boot-starter"))
 
 	testImplementation(project(":spring-testcontainers-support"))
 	testImplementation("com.redis:testcontainers-redis")
-	testImplementation("org.springframework.boot:spring-boot-data-redis")
 	testImplementation("org.springframework.boot:spring-boot-webmvc-test")
 }
