a582210b21cf845f9f336eb1a2f2019bb5572373
livk <livk.cloud@gmail.com>
2026-06-22 17:21:01 +0800

    refactor(mapstruct): 移除 mapstruct 相关模块和配置
    - 从文档中删除了 mapstruct 相关描述和示例内容
    - 清理了 spring-extension-context 下的 mapstruct 组件代码，删除所有相关类、接口和测试
    - 移除 spring-boot-extension-autoconfigure 和 spring-boot-extension-starters 中的 mapstruct 依赖与配置
    - 删除 spring-boot-extension-tests 中 mapstruct 相关的测试模块和配置文件
    - 调整 buildSrc 编译参数，去除对 mapstruct 注解处理器的特殊编译参数支持
    - 更新依赖版本管理，删除 mapstruct 及其相关依赖声明
    - 修改文档注解相关内容，删减对 mapstruct 的引用和适配说明
    
    Signed-off-by: livk <livk.cloud@gmail.com>

--- spring-boot-extension-autoconfigure/spring-boot-extension-mapstruct/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-description = "spring boot extension mapstruct"
-
-dependencies {
-	api(project(":spring-extension-context:spring-extension-mapstruct"))
-}
--- spring-extension-context/spring-extension-mapstruct/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-description = "spring boot extension mapstruct"
-
-dependencies {
-	api("org.mapstruct:mapstruct")
-	api("org.springframework:spring-context")
-}
